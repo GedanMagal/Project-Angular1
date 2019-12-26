402ce1a03a2a15f938a325a578a4015d0ab37fc4
Gedan Magalhães <gedanycar@gmail.com>
2019-12-25 23:56:02 -0300

    Corrigindo rota 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'contato', component: ContatoComponent },
-    { path: 'sobre', component: SobreComponent}
+    { path: 'sobre', component: SobreComponent},
+    {path: '404', component: NotFoundComponent},
+    {path: '**', redirectTo: '/404'}
 ];
 
---
+++ src/app/navegacao/not-found/not-found.component.html
@@ -0,0 +1,1 @@
+Pagina Não encontrada
